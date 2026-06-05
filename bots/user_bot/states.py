3a1ffa339fc9216d64919416a33e68734d9b2ace
Yevhenii Kucheruk <zhenya.kucheruk515@gmail.com>
2026-06-05 07:57:34 +0300

    editing registration

--- bots/user_bot/states.py
+++ bots/user_bot/states.py
@@ -5,6 +5,7 @@ class Registration(StatesGroup):
     choosing_language = State()  # вибір мови
     entering_name = State()     # введення імені
     entering_phone = State()    # введення номеру телефона
+    choosing_role = State()
 
 
 class SettingsStates(StatesGroup):
